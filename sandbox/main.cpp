b5de0d6dcdb38ac30650732f5d0da350ffb4e35e
Tom Hunt <tomjhunt13@gmail.com>
2020-02-14 11:41:04 +0000

    Test example calls child twice if it has multiply dependencies but this shouldn't be an issue

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -58,6 +58,43 @@ int main() {
     p7.RegisterInputSignal(s57);
 
 
+    // Second graph
+    pfunc r1("r1");
+    pfunc r2("r2");
+    pfunc r3("r3");
+    pfunc r4("r4");
+    pfunc r5("r5");
+    pfunc r6("r6");
+    pfunc r7("r7");
+    pfunc r8("r8");
+    pfunc r9("r9");
+    pfunc r10("r10");
+    pfunc r11("r11");
+
+    SimFramework::Signal t13(&r1);
+    SimFramework::Signal t14(&r1);
+    SimFramework::Signal t24(&r2);
+    SimFramework::Signal t35(&r3);
+    SimFramework::Signal t46(&r4);
+    SimFramework::Signal t57(&r5);
+    SimFramework::Signal t67(&r6);
+    SimFramework::Signal t711(&r7);
+    SimFramework::Signal t811(&r8);
+    SimFramework::Signal t910(&r9);
+    SimFramework::Signal t1011(&r10);
+
+    r3.RegisterInputSignal(t13);
+    r4.RegisterInputSignal(t14);
+    r4.RegisterInputSignal(t24);
+    r5.RegisterInputSignal(t35);
+    r6.RegisterInputSignal(t46);
+    r7.RegisterInputSignal(t57);
+    r7.RegisterInputSignal(t67);
+    r11.RegisterInputSignal(t711);
+    r11.RegisterInputSignal(t811);
+    r11.RegisterInputSignal(t1011);
+    r10.RegisterInputSignal(t910);
+
 
 
 
