75e8dae8898e9f6ba248dc292a4aa18d26e6ba94
Tom Hunt <tomjhunt13@gmail.com>
2020-02-22 18:27:01 +0000

    Working 2D interp!

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -10,6 +10,13 @@
 #include "OutputBlock.h"
 
 
+struct Table2D
+{
+    std::vector<float> x;
+    std::vector<float> y;
+    std::vector<std::vector<float>> z;
+};
+
 struct InsertionResult1D
 {
     int x1;
@@ -42,13 +49,51 @@ InsertionResult1D Insertion1D(std::vector<float>& sortedArray, float element)
 };
 
 
+struct coord2D{
+    float x, y, z;
+};
+
+
+float LinearInterp(float x, float x1, float x2, float y1, float y2)
+{
+    return y1 + ((x - x1) / (x2 - x1)) * (y2 - y1);
+}
+
+float BilinearInterp(float x, float y, coord2D x1y1, coord2D x1y2, coord2D x2y1, coord2D x2y2)
+{
+
+    // Interpolate to find f(x1, y), f(x2, y)
+    float f1 = LinearInterp(y, x1y1.y, x1y2.y, x1y1.z, x1y2.z);
+    float f2 = LinearInterp(y, x2y1.y, x2y2.y, x2y1.z, x2y2.z);
+
+    return LinearInterp(x, x1y1.x, x2y2.x, f1, f2);
+}
+
+
+float InterpTable2D(Table2D& table, float x, float y)
+{
+    // Get indices
+    InsertionResult1D xIndices = Insertion1D(table.x, x);
+    InsertionResult1D yIndices = Insertion1D(table.y, y);
+
+    coord2D x1y1 = {table.x[xIndices.x1], table.y[yIndices.x1], table.z[yIndices.x1][xIndices.x1]};
+    coord2D x1y2 = {table.x[xIndices.x1], table.y[yIndices.x2], table.z[yIndices.x2][xIndices.x1]};
+    coord2D x2y1 = {table.x[xIndices.x2], table.y[yIndices.x1], table.z[yIndices.x1][xIndices.x2]};
+    coord2D x2y2 = {table.x[xIndices.x2], table.y[yIndices.x2], table.z[yIndices.x2][xIndices.x2]};
+
+    return BilinearInterp(x, y, x1y1, x1y2, x2y1, x2y2);
+}
+
+
 int main() {
 
-    std::vector<float> array = {-10.5, -3.5, 0.5, 2.0};
+    Table2D tab;
+    tab.x =  {1.5, 2.5, 4.f};
+    tab.y = {0.5, 1.2, 2.2};
+    tab.z = {{6.f, 5.f, 1.f}, {1.f, 4.f, 5.f}, {2.f, 3.f, 2.f}};
 
-    InsertionResult1D res = Insertion1D(array, -0.2);
+    std::cout << InterpTable2D(tab, 1.5, 2.1) << std::endl;
 
-    std::cout << "X1: " << res.x1 << ", X2: " << res.x2 << std::endl;
 
 //    SimFramework::Signal<Eigen::Vector2f> signal1; // ConstantBlock States
 //    SimFramework::Signal<float> signal2; // SpringDamper Force
--- src/Interpolation.cpp
+++ src/Interpolation.cpp
@@ -11,15 +11,6 @@ struct Table2D
     std::vector<std::vector<float>> z = {{6.f, 5.f, 1.f}, {1.f, 4.f, 5.f}, {2.f, 3.f, 2.f}};
 };
 
-struct InsertionResult2D
-{
-    int x1;
-    int x2;
-
-    int y1;
-    int y2;
-};
-
 struct InsertionResult1D
 {
     int x1;
@@ -51,13 +42,18 @@ InsertionResult1D Insertion1D(std::vector<float>& sortedArray, float element)
     return {low, high};
 };
 
-
-InsertionResult2D TableInsertionSearch2D(Table2D& table, float x, float y)
-{
-
-}
-
-float LinearInterp1D(float x, float x1, float x2, float y1, float y2)
+float LinearInterp2D(float x, float y, float x1, float x2, float y1, float y2)
 {
     return ((x - x1) / (x2 - x1)) * (y2 - y1) + y1;
 }
+
+
+float InterpTable2D(Table2D& table, float x, float y)
+{
+    // Get indices
+    InsertionResult1D xIndices = Insertion1D(table.x, x);
+    InsertionResult1D yIndices = Insertion1D(table.y, y);
+
+//    return LinearInterp2D(x, y, )
+}
+
