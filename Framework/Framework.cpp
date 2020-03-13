31aba864ac2e24788a4c3a1f128be37051f7e20a
Tom Hunt <tomjhunt13@gmail.com>
2020-03-13 13:10:25 +0000

    Working tree assembly (not split into sub functions and unit tested yet)

--- Framework/Framework.cpp
+++ Framework/Framework.cpp
@@ -78,17 +78,6 @@ namespace SimFramework {
 
     namespace Internal {
 
-//        std::map<Function*, std::vector<SignalBase*>> FunctionInputs(std::vector<Function*> functions)
-//        {
-//            std::map<Function*, std::vector<SignalBase*>> outputMap;
-//            for (Function* func : functions)
-//            {
-//                outputMap.insert({func, func->InputSignals()});
-//            }
-//
-//            return outputMap;
-//        }
-
         std::map<SignalBase*, Function*> FunctionOutputs(std::vector<Function*> functions)
         {
             std::map<SignalBase*, Function*> outputMap;
@@ -102,119 +91,61 @@ namespace SimFramework {
             return outputMap;
         }
 
+        std::vector<Function*> TestRef(FunctionTree&  tree)
+        {
+            return {};
+        }
+
         std::vector<FunctionTree> AssembleTree(std::vector<Function*> functions)
         {
+
+            // Map of function pointers to their trees
+            std::vector<FunctionTree> functionTrees(functions.size());
+            std::map<Function*, FunctionTree*> mapFuncTree;
+
+            // Iterate over each function block and create corresponding tree
+            for (int i = 0; i < functions.size(); i++)
+            {
+                functionTrees[i] = {functions[i]};
+                mapFuncTree.insert({functions[i], &functionTrees[i]});
+            }
+
             // Map of signals to their driving functions
             std::map<SignalBase*, Function*> mapSigFunc = FunctionOutputs(functions);
 
-            // Map of function pointers to their trees
-            std::map<Function*, FunctionTree> mapFuncTree;
-
-            // Iterate over each function block and create corresponding tree
-            for (Function* func : functions)
-            {
-                mapFuncTree.insert({func, {func}});
-            }
-
             // Iterate over each function again to create tree structure
-            for (Function* func : functions)
+            for (FunctionTree& tree : functionTrees)
             {
 
                 // Iterate over each driving signal of function
-                for (SignalBase* signal : func->InputSignals())
+                for (SignalBase* signal : tree.block->InputSignals())
                 {
-
                     // If driving signal is driven by a function
                     if (mapSigFunc.count(signal) > 0)
                     {
-
                         // Add driving function of the driving signal as child of func function tree and mark root as false
-                        mapFuncTree[func].children.push_back(&mapFuncTree[mapSigFunc[signal]]);
-                        mapFuncTree[mapSigFunc[signal]].root = false;
+                        tree.children.push_back(mapFuncTree[mapSigFunc[signal]]);
+                        mapFuncTree[mapSigFunc[signal]]->root = false;
                     }
                 }
             }
 
-
             // Find roots of trees
             std::vector<FunctionTree> treeVector;
 
-            for (Function* func : functions)
+            for (FunctionTree& tree : functionTrees)
             {
-                if (mapFuncTree[func].root)
+                if (tree.root)
                 {
-                    treeVector.push_back(mapFuncTree[func]);
+                    treeVector.push_back(tree);
                 }
             }
 
             // If no root, algebraic loop!
+            TestRef(treeVector[0]);
 
             return treeVector;
 
-
-//        FunctionTree AssembleTree(std::map<Function*, std::vector<SignalBase*>> inputMap, std::map<SignalBase*, Function*> outputMap)
-//        {
-//            // Map of block pointers to trees
-//            std::map<Function*, FunctionTree> blockMap;
-//
-//            // Iterate over each function block and create tree
-//            for (auto input : inputMap)
-//            {
-//                blockMap.insert({std::get<0>(input), {std::get<0>(input)}});
-//            }
-//
-//            // Iterate over each function again and create tree structure
-//            for (auto input : inputMap)
-//            {
-//
-//                // Iterate over each child signal of function
-//                for (auto signal : std::get<1>(input))
-//                {
-//
-//                    // Get driving function block
-//
-//
-//                }
-//
-//            }
-//
-//
-//            // If no root, algebraic loop!
-//
-//
-//
-//            return {};
-
-//            // Iterate over each function block again to fill tree with dependants
-//            for (Block* func : this->m_Functions)
-//            {
-//                for (Block* b : func->InputBlocks())
-//                {
-//                    if (b->BlockType() == e_BlockType ::eFunction)
-//                    {
-//                        blockMap[func].children.push_back(&blockMap[b]);
-//                        blockMap[b].root = false;
-//                    }
-//                }
-//            }
-//
-//            // Create ordered list
-//            std::vector<Block*> orderedList;
-//            for (Block* func : this->m_Functions)
-//            {
-//
-//                if (blockMap[func].root) {
-//
-//                    // Depth first traversal from root
-//                    std::vector<Block*> treeList;
-//                    SortTree(&blockMap[func], treeList);
-//
-//                    // Add to ordered list
-//                    orderedList.insert(orderedList.end(), treeList.begin(), treeList.end());
-//                }
-//            }
-//
-//            this->m_Functions = orderedList;
         }
 
     }; // namespace Internal
--- Test/test_Framework.cpp
+++ Test/test_Framework.cpp
@@ -7,7 +7,49 @@
 #include "SimFramework/Components.h"
 
 
-TEST(AssembleTree, test1) {
+//TEST(AssembleTree, TwoSerialTrees) {
+//    // Signals
+//    SimFramework::Signal<float> s1;
+//    SimFramework::Signal<float> s2;
+//    SimFramework::Signal<float> s3;
+//    SimFramework::Signal<float> s4;
+//    SimFramework::Signal<float> s5;
+//    SimFramework::Signal<float> s6;
+//
+//    // Blocks
+//    SimFramework::Gain<float, float> a;
+//    SimFramework::Gain<float, float> b;
+//    SimFramework::Gain<float, float> c;
+//    SimFramework::Gain<float, float> d;
+//
+//    a.Configure(&s1, &s2, 1);
+//    b.Configure(&s2, &s3, 1);
+//    c.Configure(&s4, &s5, 1);
+//    d.Configure(&s5, &s6, 1);
+//
+//
+//    std::vector<SimFramework::Function*> funcs = {&a, &b, &c, &d};
+//    std::vector<SimFramework::Internal::FunctionTree> ft = SimFramework::Internal::AssembleTree(funcs);
+//
+//    // Assertions
+//    ASSERT_EQ(ft.size(), 2);
+//    ASSERT_TRUE(ft[0].block == &b || ft[0].block == &d);
+//    ASSERT_TRUE(ft[1].block == &b || ft[1].block == &d);
+//
+//    if (ft[0].block == &b)
+//    {
+//        ASSERT_TRUE(ft[0].children[0]->block == &a);
+//        ASSERT_TRUE(ft[1].children[0]->block == &c);
+//    }
+//
+//    else if (ft[0].block == &d)
+//    {
+//        ASSERT_TRUE(ft[0].children[0]->block == &c);
+//        ASSERT_TRUE(ft[1].children[0]->block == &a);
+//    }
+//}
+
+TEST(AssembleTree, SummingJunction) {
     // Signals
     SimFramework::Signal<float> s1;
     SimFramework::Signal<float> s2;
@@ -15,32 +57,25 @@ TEST(AssembleTree, test1) {
     SimFramework::Signal<float> s4;
     SimFramework::Signal<float> s5;
     SimFramework::Signal<float> s6;
+    SimFramework::Signal<float> s7;
 
     // Blocks
     SimFramework::Gain<float, float> a;
     SimFramework::Gain<float, float> b;
-    SimFramework::Gain<float, float> c;
+    SimFramework::SummingJunction<float> c;
     SimFramework::Gain<float, float> d;
 
     a.Configure(&s1, &s2, 1);
-    b.Configure(&s2, &s3, 1);
-    c.Configure(&s4, &s5, 1);
-    d.Configure(&s5, &s6, 1);
-
-
-    SimFramework::Function* pb  = &b;
-    SimFramework::Function* pd  = &d;
-
-
+    b.Configure(&s3, &s4, 1);
+    c.Configure({&s2, &s4, &s5}, &s6, {1.f, 1.f, 1.f});
+    d.Configure(&s6, &s7, 1);
 
     std::vector<SimFramework::Function*> funcs = {&a, &b, &c, &d};
     std::vector<SimFramework::Internal::FunctionTree> ft = SimFramework::Internal::AssembleTree(funcs);
 
     // Assertions
-    ASSERT_EQ(ft.size(), 2);
-
-    ASSERT_TRUE(ft[0].block == &b || ft[0].block == &d);
-    ASSERT_TRUE(ft[1].block == &b || ft[1].block == &d);
-
-    int kja = 1;
+    ASSERT_EQ(ft.size(), 1);
+    ASSERT_TRUE(ft[0].block == &d);
+    ASSERT_TRUE(ft[0].children[0]->block == &c);
+    ASSERT_EQ(ft[0].children[0]->children.size(), 2);
 }
