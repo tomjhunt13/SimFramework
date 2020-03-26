b26acd2c5c7184a308a8f144a5d6e2207516f7cd
Tom Hunt <tomjhunt13@gmail.com>
2020-03-26 15:17:36 +0000

    Updated MassSpringDamper1D to new system

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-
-#include "SimFramework/Components.h"
-
-
 #include "SandboxMassSpringDamper.h"
 #include "SandboxEngine.h"
 #include "SandboxTransmission.h"
@@ -12,28 +7,8 @@
 
 int main() {
 
-    SimFramework::Input<float> cnst;
-    cnst.Configure(2.4);
-    cnst.Initialise(0.f);
-
-    cnst.WriteValue(4.f);
-    cnst.Update(0.f);
-
-
-    SimFramework::Gain<float, float> g;
-
-    const SimFramework::Signal<float>* out = g.OutSignal();
-    SimFramework::Signal<float> temp;
-
-    g.Configure(cnst.OutSignal(), &temp, 3.f);
-
-    g.Update();
-
-    std::cout << out->Read() << std::endl;
-
-
 //    SandboxFullModel();
-//    SandboxMassSpringDamper();
+    SandboxMassSpringDamper();
 //    SandboxEngine();
 //    SandboxTransmission();
 //    SandboxVehicleStandalone();
--- SimModels/include/SimModels/MassSpringDamper1D.h
+++ SimModels/include/SimModels/MassSpringDamper1D.h
@@ -20,12 +20,7 @@ namespace Models {
 
     public:
         MassSpringDamper1D(float mass=1.f, float k=1.f, float c=1.f);
-
         MassSpringDamperBlocks Blocks();
-
-        SimFramework::Input<float>* InputForceBlock();
-        SimFramework::Output<float>* MassPositionBlock();
-        SimFramework::Output<float>* MassVelocityBlock();
 
     private:
 
@@ -34,24 +29,22 @@ namespace Models {
         float m_K;
         float m_C;
 
-        // Signals
-        SimFramework::Signal<float> m_InputForce;
-        SimFramework::Signal<Eigen::Vector<float, 1>> m_SpringDamperForce_Vec;
-        SimFramework::Signal<float> m_SpringDamperForce;
-        SimFramework::Signal<float> m_SummedForce;
-        SimFramework::Signal<Eigen::Vector2f> m_MassStates;
-        SimFramework::Signal<float> m_MassPosition;
-        SimFramework::Signal<float> m_MassVelocity;
-
         // Blocks
         SimFramework::Input<float> m_Input;
-        SimFramework::Gain<Eigen::Vector2f, Eigen::Vector<float, 1>, Eigen::Matrix<float, 1, 2>> m_Gain;
-        SimFramework::Mask<Eigen::Vector<float, 1>, float, 1> m_Mask1;
-        SimFramework::SummingJunction<float> m_SumForces;
-        SimFramework::StateSpace<float, Eigen::Vector2f, 1, 2, 2> m_MassBlock;
-        SimFramework::Mask<Eigen::Vector2f, float, 2> m_Mask2;
+        SimFramework::StateSpace<float, Eigen::Vector2f, 1, 2, 2> m_StateSpace;
+        SimFramework::Mask<Eigen::Vector2f, float, 2> m_StateMask;
         SimFramework::Output<float> m_PositionOutputBlock;
         SimFramework::Output<float> m_VelocityOutputBlock;
+
+
+
+
+//        SimFramework::Gain<Eigen::Vector2f, Eigen::Vector<float, 1>, Eigen::Matrix<float, 1, 2>> m_Gain;
+//        SimFramework::Mask<Eigen::Vector<float, 1>, float, 1> m_Mask1;
+//        SimFramework::SummingJunction<float> m_SumForces;
+//        SimFramework::StateSpace<float, Eigen::Vector2f, 1, 2, 2> m_MassBlock;
+//        SimFramework::Mask<Eigen::Vector2f, float, 2> m_Mask2;
+
     };
 
 }; // namespace SimModels
